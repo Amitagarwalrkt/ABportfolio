75ef7dec7e599363b612cbb2523531c85e77cd51
Amitagarwalrkt <107742055+Amitagarwalrkt@users.noreply.github.com>
2023-11-01 21:28:49 +0530

    Add files via upload

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi Everyone, I am Aanchal Bisht from Haldwani, Uttarakhand, India. I am a computer science graduate and a passionate programmer. In addition to coding, here are some other things I enjoy doing!"
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
